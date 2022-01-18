a19979e529fe317143a61427acd4085e07f5bbff
Henry <lihenryhl@yahoo.com>
2022-01-18 01:33:04 -0800

    getting more react practice

--- src/content.js
+++ src/content.js
@@ -13,10 +13,23 @@ const Content = () => {
             name:'jack'
         }
     ]);
+
+    const [input, setInput] = useState('');
+
     const handleClick = (id) =>{
-        console.log({...items})
         const duplicate = items.map((item) => item.id === id ? {...item,checked:!item.checked}:item);
         setItems(duplicate)
+        localStorage.setItem('list', JSON.stringify(duplicate));
+    }
+
+    const inputHandler = (event) => {
+        console.log(event.target.value)
+        setInput(event.target.value)
+    }
+
+    const deleteHandler = (id) => {
+        const ss = items.filter((item) => item.id !== id);
+        setItems(ss)
     }
 
     return (
@@ -26,8 +39,11 @@ const Content = () => {
                 <li key = {item.id}>
                     <input type = "checkbox" checked = {item.checked} onChange = {() => handleClick(item.id)}/>
                     <span>{item.name}</span>
+                    <button onClick ={()=> deleteHandler(item.id)}>Delete</button>
                 </li>
             )}
+            <input onChange = {(e)=> inputHandler(e)}/>
+            <div>{input}</div>
             </ul>
         </main>
     )
